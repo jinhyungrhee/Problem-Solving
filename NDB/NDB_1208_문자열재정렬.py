9fcd489c1d8ae0404646979d9ccbce7f64ec1d1f
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-02-06 09:50:58 +0900

    NDB_1208_문자열재정렬(Implementation)

---
+++ NDB/NDB_1208_문자열재정렬.py
@@ -0,0 +1,18 @@
+'''
+#1 배열 하나와 isalpha() 사용하여 해결 가능!
+'''
+
+string = input()
+
+ascii = []
+number = []
+for i in string:
+  if ord(i) >= 65 and ord(i) <= 90:
+    ascii.append(i)
+  else:
+    number.append(int(i))
+
+sorted(ascii)
+res_str = ''.join(sorted(ascii))
+res_num = sum(number)
+print(res_str + str(res_num))
