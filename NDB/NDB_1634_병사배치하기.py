e80e15eb9a13df5313545776039a3d219f39f88e
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-03-08 10:11:26 +0900

    NDB_1634_병사배치하기(DP, LIS)

--- NDB/NDB_1634_병사배치하기.py
+++ NDB/NDB_1634_병사배치하기.py
@@ -5,7 +5,7 @@
 n = int(input())
 
 array = list(map(int, input().split()))
-# 가장 긴 감소 부분 수열 문제로 reverse
+# 최대 증가 부분 수열 문제(LIS)로 만들기 위해 reverse
 array.reverse()
 
 # 1차원 dp 테이블
@@ -13,8 +13,8 @@ dp = [1] * n
 
 for i in range(1, n):
   for j in range(0, i):
-    if array[j] < array[i]:
-      dp[i] = max(dp[i], dp[j] + 1) # dp[i]에 계속 값이 갱신됨
+    if array[j] < array[i]: # 기준 값보다 앞에 값이 작으면(LIS)
+      dp[i] = max(dp[i], dp[j] + 1) # DP 수행(안쪽 for문을 돌면서 dp[i]에 계속 값이 갱신되어 결국 최대값으로 저장됨)
   
-# 열외시켜야 하는 병사의 최소 수 (= 전체 길이 - 최장 감소 부분 수열의 길이)
+# 열외시켜야 하는 병사의 최소 수(= 전체 길이 - 최장 감소 부분 수열의 길이)
 print(n - max(dp))
