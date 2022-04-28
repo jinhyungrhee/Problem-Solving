c3b95f0850a012a65272142eeac8cb686b8d7508
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-04-29 00:09:28 +0900

    NDB_1101_모험가길드(Greedy)[2트]

--- NDB/NDB_1101_모험가길드.java
+++ NDB/NDB_1101_모험가길드.java
@@ -1,36 +1,29 @@
 import java.util.*;
-public class explorerGuild {
 
-    public static int n;
-//    public static int[] Arr = new int[100000];
-    public static ArrayList<Integer> arrayList = new ArrayList<>(); // 가변길이 리스트
+class Main{
+  static int n;
+  static ArrayList<Integer> arrayList = new ArrayList<>();
+  
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
 
-//        int n = sc.nextInt();
-        n = sc.nextInt();
-
-        for (int i = 0; i < n; i++) {
-//            Arr[i] = sc.nextInt();
-            arrayList.add(sc.nextInt());
-        }
-
-//        Arrays.sort(Arr);
-        Collections.sort(arrayList);
-
-        int totalGroup = 0; // 총 그룹수
-        int gMember = 0; // 현재 그룹에 포함된 모험가의 수
-        for (int i = 0; i < n; i++) { // 공포도 낮은 것부터 하나씩 확인
-            gMember += 1; // 현재 그룹에 해당 모험가 포함시킴
-            if (gMember >= arrayList.get(i)) { // 현재 그룹에 포함된 모험가의 수가 현재 공포도 이상이면 그룹 결성
-                totalGroup += 1; // 총 그룹 수 증가
-                gMember = 0; // 현재 그룹에 포함된 모험가의 수 초기화
-            }
-            
-        }
-
-        System.out.println(totalGroup);
-
+    for (int i = 0; i < n; i++) {
+      arrayList.add(sc.nextInt());
     }
+
+    Collections.sort(arrayList);
+
+    int gNum = 0;
+    int mNum = 0;
+    for (int i = 0; i < n; i++) {
+      mNum += 1;
+      if (arrayList.get(i) <= mNum) {
+        mNum = 0; // 멤버 수 초기화
+        gNum += 1; // 그룹 하나 결성
+      }
+    }
+    System.out.println(gNum);
+  }
 }
