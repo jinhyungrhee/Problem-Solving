3d7e7cc2380b93393f3af2786dd14bade3994e3b
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-06-02 16:42:56 +0900

    6_연속된 자연수의 합(투 포인터)

---
+++ Doit/1_자료구조/6_연속된자연수의합.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+class Main{
+  
+  public static void main(String[] args) throws IOException {
+    Scanner sc = new Scanner(System.in);
+
+    int N = sc.nextInt();
+    
+    // 사용 변수 초기화
+    int count = 1;
+    int start_index = 1;
+    int end_index = 1;
+    int sum = 1;
+
+    while (end_index != N ) {
+
+      if (sum == N) {
+        count++;
+        end_index++;
+        sum = sum + end_index;
+      } else if(sum > N) {
+        sum = sum - start_index;
+        start_index++;
+      } else {
+        end_index++;
+        sum = sum + end_index;
+      }
+    }
+    System.out.println(count);
+  }
+}
