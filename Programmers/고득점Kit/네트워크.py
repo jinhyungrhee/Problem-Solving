0a77a99d556127fd512365e1c99d194d428fceaa
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-06-23 22:43:27 +0900

    네트워크(DFS, BFS)

--- Programmers/고득점Kit/네트워크.py
+++ Programmers/고득점Kit/네트워크.py
@@ -1,10 +1,58 @@
 from collections import deque
+# IDEA : 네트워크의 개수는 곧 트리의 개수임. 한 노드에서 탐색을 시작해 최대 방문한 것을 1개의 트리로 계산!
 
+# 모범답안 DFS
+def solution(n, computers):
+    answer = 0
+    visited = [False for i in range(n)]
+    
+    for computer in range(n):
+        if not visited[computer]:
+            DFS(n, computers, computer, visited)
+            answer += 1 # 총 DFS를 수행한 횟수를 체크
+            
+    return answer
+
+def DFS(n, computers, now, visited):
+    visited[now] = True
+    for next in range(n):
+        # 자신이 아니면서 현재 노드와 연결된 컴퓨터이면서
+        if next != now and computers[now][next] == 1:
+            if not visited[next]: # 아직 방문하지 않았다면
+                DFS(n, computers, next, visited) # DFS 수행
+
+# 모범답안 BFS
 def solution(n, computers):
     answer = 0
     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
+    visited = [False for _ in range(n)]
+    
+    for computer in range(n):
+        if not visited[computer]:
+            BFS(n, computers, computer, visited)
+            answer += 1 # BFS가 수행된 횟수가 곧 트리(=네트워크)의 개수임
+    
+    return answer
+
+
+def BFS(n, computers, now, visited):
+    visited[now] = True
+    queue = deque([now])
+    
+    while queue:
+        now = queue.popleft()
+        for next in range(n): # 인접 노드 탐색
+            if now != next and computers[now][next] == 1:
+                if not visited[next]:
+                    visited[next] = True
+                    queue.append(next)
+
+
+# 내 답안
+'''
+def solution(n, computers):
+    answer = 0
+    
     visited = [False for _ in range(n + 1)]
     network = [0 for _ in range(n + 1)] # 네트워크 번호 기록
     graph = [[] for _ in range(n + 1)] # 방문여부 기록
@@ -37,3 +85,4 @@ def solution(n, computers):
     network = set(network) - set(range(1))
     answer = len(network)
     return answer
+'''
