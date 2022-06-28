d02e59cab6b0a330a28a1387280c1c8dbc5b9d6c
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-06-28 22:57:08 +0900

    NDB_1208_문자열 재정렬(Implementation)

--- NDB/NDB_1208_문자열재정렬.py
+++ NDB/NDB_1208_문자열재정렬.py
@@ -1,7 +1,23 @@
-'''
+# 220628
+
+data = input()
+
+alpha = []
+num = 0
+for d in data:
+  if d.isalpha():
+    alpha.append(d)
+  else:
+    num += int(d)
+
+alpha.sort()
+
+answer = ''.join(alpha) + str(num)
+
+print(answer)
+
 #1 배열 하나와 isalpha() 사용하여 해결 가능!
 '''
-
 string = input()
 
 ascii = []
@@ -16,3 +32,4 @@ sorted(ascii)
 res_str = ''.join(sorted(ascii))
 res_num = sum(number)
 print(res_str + str(res_num))
+'''
