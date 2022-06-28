a92679a68093b155bd304a337047bef95dee1363
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-06-28 23:19:17 +0900

    NDB_1209_문자열 압축(Implementation, slicing)

--- NDB/NDB_1209_문자열압축.py
+++ NDB/NDB_1209_문자열압축.py
@@ -1,3 +1,32 @@
+# 220628
+
+s = 'abcabcabcabcdededededede'
+
+answer = len(s) # 처음 문자열 길이
+
+for step in range(1, len(s)//2 + 1):
+  compressed = ''
+  prev = s[0:step] # 이전 문자열
+  # print(prev)
+  count = 1
+  for j in range(step, len(s), step):
+    if prev == s[j:j+step]: # s[j:j+step] - 현재 문자열
+      count += 1
+    else: # 이전 문자열과 현재 문자열이 다르면
+      # 지금까지 압축한 내용 저장
+      compressed += str(count) + prev if count >= 2 else prev
+      prev = s[j:j+step] # 현재 문자열을 이전 문자열로 변경
+      count = 1
+
+  # j-for문이 종료된 후에 마지막 압축 내용이 반영되지 않을 수 있음
+  compressed += str(count) + prev if count >= 2 else prev
+
+  answer = min(answer, len(compressed))
+  print(compressed)
+
+print(answer)
+
+'''
 def solution(word):
   n = len(word)
   # step 1부터 비교
@@ -24,3 +53,4 @@ def solution(word):
 
 word = "aaaabbabbabb"
 print(solution(word))
+'''
