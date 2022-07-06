c3a128b67513a797e5ab28dfc1128c72e31ace50
Jinhyung Rhee <rheejinhyung@gmail.com>
2022-07-06 12:04:34 +0900

    BOJ_1927_최소힙(Implementation, Heap)

---
+++ BOJ/BOJ_1927_최소힙.py
@@ -0,0 +1,16 @@
+import heapq, sys
+
+n = int(sys.stdin.readline())
+
+heap = []
+
+for _ in range(n):
+  num = int(sys.stdin.readline())
+  if num == 0:
+    if len(heap) == 0:
+      print(0)
+    else:
+      min_val = heapq.heappop(heap)
+      print(min_val)
+  else:
+    heapq.heappush(heap, num)
