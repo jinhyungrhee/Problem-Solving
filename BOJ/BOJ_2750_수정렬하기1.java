123a58c5b4846992ed6d6a97d5c9aaea7d8524a1
Jinhyung Rhee <rheejinhyung@gmail.com>
2023-01-08 21:35:14 +0900

    BOJ_2750_수 정렬하기1 (Bubble Sort - O(N^2))

---
+++ BOJ/BOJ_2750_수정렬하기1.java
@@ -0,0 +1,29 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+  
+  public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+    int N = sc.nextInt();
+    int[] A = new int[N];
+
+    for (int i = 0; i < N; i++) {
+      A[i] = sc.nextInt();
+    }
+
+    for (int i = 0; i < N - 1; i++) {
+      for (int j = 0; i < N - i - 1; j++) {
+        if (A[j] > A[j + 1]) {
+          int temp = A[j];
+          A[j] = A[j + 1];
+          A[j + 1] = temp;
+        }
+      }
+    }
+    System.out.println(Arrays.toString(A));
+  }
+}
+
+// Bubble Sort -> O(n^2)
